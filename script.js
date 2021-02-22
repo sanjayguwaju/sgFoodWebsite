c8ecb7cd6465f49a013f1be3a17e811ef163cb2d
sanjayguwaju <sanjayguwaju@gmail.com>
2021-02-22 19:52:09 +0545

    Menu has been fixed and hover effect and opacity added

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+document.querySelector('.menu').addEventListener
+('click',()=>{
+  document.querySelectorAll('.target').forEach((item)=>{
+    item.classList.toggle('change')
+  })
+})
+
+
+
 const icons = document.querySelectorAll('.section-1-icons i');
 
 let i = 1
